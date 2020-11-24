a17abc7f2a37cb3bc0fad71e00c3df9b8c4e62fc
zhangchao <529743595@qq.com>
2020-11-24 13:37:06 +0800

    feat: test reactive

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  {{year.a}}
+  {{year.bb}}
+  {{message}}
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, reactive, ref, onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+type Year = {
+  a: number;
+  bb?: number;
 }
+
+export default defineComponent({
+  components: {
+    HelloWorld,
+  },
+  setup(props) {
+    const count = ref(10)
+    const year = reactive<Year>({a: 2020})
+    onMounted(() => {
+      console.log("mounted vue3 typescript", 11111111);
+      setTimeout(() => {
+        count.value = 3
+        year.bb = 1000
+      }, 1000)
+    })
+    return {
+      count,
+      year,
+    }
+  },
+  
+})
+
 </script>
