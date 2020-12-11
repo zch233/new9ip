81ebdc895260dabec2fcc152baed2d79c2c69e0e
zhangchao <529743595@qq.com>
2020-12-11 17:35:47 +0800

    feat: wrap antd components

---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <AntInput>
+    <template #prefix>
+      <slot name="prefix"></slot>
+    </template>
+    <slot></slot>
+  </AntInput>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Input} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'MyInput',
+  components: {AntInput: Input}
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/UI/MyTabPane.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntTabPane>
+    <slot></slot>
+  </AntTabPane>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Tabs} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'MyTabPane',
+  components: {AntTabPane: Tabs.TabPane}
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
