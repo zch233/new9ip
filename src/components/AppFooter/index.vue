4c6464945b2c530df7aa059cbffc490d414e1125
zhangchao <529743595@qq.com>
2020-12-16 17:45:14 +0800

    feat: add app footer

---
+++ src/components/AppFooter/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <footer class="appFooter">
+    <ul class="pageWidthWithCenter footerList">
+      <li v-for="item in footerInfoList" :key="item.title" class="footerItem">
+        <p class="footerItem-title">{{item.title}}</p>
+        <p class="footerItem-content" v-for="contentItem in item.content">
+          <RouterLink to="/">{{contentItem}}</RouterLink>
+        </p>
+      </li>
+      <li class="footerItem">
+        <p class="footerItem-title">微信公众号</p>
+        <img src="../../assets/page/qrcode.jpg" alt="">
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppFooter',
+  setup() {
+    const footerInfoList = [
+      {
+        title: '服务内容',
+        content: ['专利交易','商标交易','企业服务','专利增值服务','商标增值服务'],
+      },
+      {
+        title: '交易保障',
+        content: ['支付方式','签署法律合同','服务协议','法律顾问'],
+      },
+      {
+        title: '常见问题',
+        content: ['专利申请','专利购买','发明专利','实用新型专利', '外观设计专利'],
+      },
+    ]
+    return {
+      footerInfoList
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.appFooter {
+  background-color: #2E3547;
+  color: #999999;
+  font-size: 12px;
+  padding: 4% 0;
+  .footerList {
+    padding: 0 5%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+  .footerItem {
+    list-style: none;
+    &-title {color: #fff;margin-bottom: 8px;font-size: 14px}
+    &-content {margin-bottom: 6px;a {color: inherit;&:hover {color: #14A8BD;}}}
+  }
+}
+</style>
