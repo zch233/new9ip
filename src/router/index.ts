4bbec50697b3f0da95fb047b835aec9c94bf0764
zhangchao <529743595@qq.com>
2020-12-16 17:44:22 +0800

    refactor: fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,31 +5,42 @@ export const router = createRouter({
   routes: [
     {
       path: '/',
-      meta: { title: '首页' },
-      component: () => import('../views/Home/index.vue'),
-    },
-    {
-      path: '/patent',
-      meta: { title: '专利列表' },
-      component: () => import('../views/Patent/index.vue'),
+      component: () => import('../views/Layout/App.vue'),
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          meta: { title: '首页' },
+          component: () => import('../views/Home/index.vue'),
+        },
+        {
+          path: '/patent',
+          name: 'Patent',
+          meta: { title: '专利列表' },
+          component: () => import('../views/Patent/index.vue'),
+        },
+      ]
     },
     {
       path: '/auth',
-      component: () => import('../views/Auth/index.vue'),
+      component: () => import('../views/Layout/Auth.vue'),
       redirect: '/auth/sign_in',
       children: [
         {
           path: 'sign_in',
+          name: 'AuthSignIn',
           meta: { title: '欢迎注册' },
           component: () => import('../views/Auth/SignIn.vue')
         },
         {
           path: 'sign_up',
+          name: 'AuthSignUp',
           meta: { title: '欢迎登录' },
           component: () => import('../views/Auth/SignUp.vue')
         },
         {
           path: 'password',
+          name: 'AuthPassword',
           meta: { title: '忘了密码' },
           component: () => import('../views/Auth/Password.vue')
         },
