897f103bfe5db73cd484da520962dbc4a600e0c2
zhangchao <529743595@qq.com>
2020-12-16 11:31:54 +0800

    styles: add button styles

--- src/components/UI/UIButton.vue
+++ src/components/UI/UIButton.vue
@@ -21,10 +21,21 @@ export default defineComponent({
   .mainButton {
     background-color: #14A8BD;
     border-color: #14A8BD;
-    &.ant-btn-primary.active, &.ant-btn-primary:active {}
-
     &.ant-btn-primary:hover, &.ant-btn-primary:focus {
-
+      background-color: #16B5CB;
+    }
+    &.ant-btn-primary.active, &.ant-btn-primary:active {
+      background-color: #1199AD;
+    }
+  }
+  .dangerButton {
+    background-color: #FF5858;
+    border-color: #FF5858;
+    &.ant-btn-primary:hover, &.ant-btn-primary:focus {
+      background-color: #FF7575;
+    }
+    &.ant-btn-primary.active, &.ant-btn-primary:active {
+      background-color: #F14141;
     }
   }
 </style>
