0f869cd44cc2eb7f81fe4711d3d521bd975389b1
zhangchao <529743595@qq.com>
2020-12-17 15:03:45 +0800

    feat: add ui menu components

---
+++ src/components/UI/UIMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntMenu>
+    <slot></slot>
+  </AntMenu>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Menu} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'UIMenu',
+  components: {AntMenu: Menu},
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/UI/UIMenuItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntMenuItem>
+    <slot></slot>
+  </AntMenuItem>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Menu} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'UIMenuItem',
+  components: {AntMenuItem: Menu.Item},
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
