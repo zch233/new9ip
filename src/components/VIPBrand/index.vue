7c7c0bea2b7b599fb74b123ca91c20a40996010e
zhangchao <529743595@qq.com>
2020-12-17 11:25:03 +0800

    feat: add vip brand

---
+++ src/components/VIPBrand/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <span class="VIPBrandWrapper">
+    <AIcon class="VIPBrand" icon="VIP" />
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Icon from '/@components/Icon/index.vue'
+
+export default defineComponent({
+  name: 'Icon',
+  components: {AIcon: Icon},
+})
+</script>
+
+<style lang="scss" scoped>
+.VIPBrandWrapper {
+  display: inline-flex;
+  overflow: hidden;
+  height: .5em;
+  padding: 0 .1em;
+  align-items: center;
+  vertical-align: -.36em;
+  border-radius: 3px;
+}
+.VIPBrand {
+  background-image: linear-gradient(90deg, #FF8D25 0%, #FFCA8A 100%);
+}
+</style>
