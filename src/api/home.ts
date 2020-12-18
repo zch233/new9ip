47eddee9ad181a7a05dea278cc074dacd518a2ca
zhangchao <529743595@qq.com>
2020-12-18 16:41:44 +0800

    feat: add search api

--- src/api/home.ts
+++ src/api/home.ts
@@ -20,3 +20,15 @@ export const getHotSearchKeywords = () =>
     url: '/pub/api/v1/search/hotRank',
     method: 'get',
   })
+
+export const getSearchHistory = () =>
+  request({
+    url: '/pub/api/v1/search/history',
+    method: 'get',
+  })
+
+export const deleteSearchHistory = (word: string) =>
+  request({
+    url: `/pub/api/v1/search/history/${word}`,
+    method: 'delete',
+  })
