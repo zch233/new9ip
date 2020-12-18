bd1d8903a0ce8ce712a592584448f577c4d245f5
zhangchao <529743595@qq.com>
2020-12-18 17:37:26 +0800

    feat: add patent detail router

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,6 +19,12 @@ export const router = createRouter({
           meta: { title: '专利列表' },
           component: () => import('../views/Patent/index.vue'),
         },
+        {
+          path: '/patent/:number',
+          name: 'PatentDetail',
+          meta: { title: '专利详情' },
+          component: () => import('../views/PatentDetail/index.vue'),
+        },
       ]
     },
     {
---
+++ src/views/PatentDetail/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>2343</p>
+  <p>2343</p>
+  <p>2343</p>
+  <p>2343</p>
+  <p>2343</p>
+  <p>2343</p>
+  <p>2343</p>
+  <p>2343</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PatentDetail',
+  setup() {
+    return {
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
