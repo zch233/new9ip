c7e709aaeec7f014f51c64535c8ad8cd046c1e06
zhangchao <529743595@qq.com>
2020-12-22 17:56:31 +0800

    feat: add ui skeleton

---
+++ src/components/UI/UISkeleton.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntSkeleton>
+    <slot></slot>
+  </AntSkeleton>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Skeleton} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'UISkeleton',
+  components: {AntSkeleton: Skeleton}
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
