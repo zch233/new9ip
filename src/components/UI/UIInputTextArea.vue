9d681c99f73eea9b422a426e9a9e36fe3998c4ea
zhangchao <529743595@qq.com>
2020-12-23 16:32:33 +0800

    feat: add ui input textarea

---
+++ src/components/UI/UIInputTextArea.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntInputTextArea>
+    <slot></slot>
+  </AntInputTextArea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Input} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'UIInputTextArea',
+  components: {AntInputTextArea: Input.TextArea},
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
