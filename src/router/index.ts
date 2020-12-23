3ea84477c15d0c9ed823ce1ca9099ee76956975f
zhangchao <529743595@qq.com>
2020-12-23 17:31:34 +0800

    feat: add pay routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -36,6 +36,30 @@ export const router = createRouter({
           name: 'OrderConfirm',
           meta: { title: '确认订单' },
           component: () => import('../views/OrderConfirm/index.vue')
+        },
+        {
+          path: 'pay/code',
+          name: 'QRCodePay',
+          meta: { title: '聚合支付' },
+          component: () => import('../views/OrderPay/QRCodePay.vue')
+        },
+        {
+          path: 'pay/wechat',
+          name: 'WechatPay',
+          meta: { title: '微信支付' },
+          component: () => import('../views/OrderPay/WechatPay.vue')
+        },
+        {
+          path: 'pay/form',
+          name: 'FormPay',
+          meta: { title: '表单支付' },
+          component: () => import('../views/OrderPay/FormPay.vue')
+        },
+        {
+          path: 'pay/result',
+          name: 'Result',
+          meta: { title: '支付结果' },
+          component: () => import('../views/OrderPay/Result.vue')
         }
       ],
     },
