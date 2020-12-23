078688e8344680753256370f3bb8ff4d0bdbe4b7
zhangchao <529743595@qq.com>
2020-12-23 11:36:36 +0800

    feat: add blank layout

---
+++ src/views/Layout/Blank.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppHeader />
+  <RouterView />
+  <AppFooter />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppFooter from '/@components/AppFooter/index.vue'
+import AppHeader from '/@components/AppHeader/index.vue'
+
+export default defineComponent({
+  name: 'LayoutBlank',
+  components:{AppFooter, AppHeader},
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
