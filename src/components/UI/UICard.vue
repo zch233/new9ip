a635c40bb921ef06cfe6ad9838cd0ef1cac1bedf
zhangchao <529743595@qq.com>
2020-12-23 13:45:12 +0800

    feat: add ui card

---
+++ src/components/UI/UICard.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntCard>
+    <slot></slot>
+  </AntCard>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Card} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'UICard',
+  components: {AntCard: Card},
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
