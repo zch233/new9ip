18a3292bab6f6aa51e8bc3d2035bf10f754c1b87
zhangchao <529743595@qq.com>
2020-12-28 19:12:51 +0800

    feat: add router before

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,13 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, onMounted } from 'vue';
 import {ConfigProvider} from 'ant-design-vue'
 import zhCN from '../node_modules/ant-design-vue/es/locale/zh_CN';
 import PageFooter from '/@components/PageFooter/index.vue'
 import { useRoute } from 'vue-router';
+import * as authApi from '/@api/auth';
+import { useStore } from '/@/store';
 
 export default defineComponent({
   name: 'App',
@@ -20,7 +22,18 @@ export default defineComponent({
   },
   setup() {
     const route = useRoute()
+    const store = useStore()
     const authRoute = computed(() => route.path.indexOf('/auth/') >= 0)
+    onMounted(() => {
+      document.title = import.meta.env.VITE_APP_TITLE + ' - ' + route.meta.title;
+      authApi.getUserDefault().then(({ data }) => {
+        store.commit('COMMIT_USER', data.data || {})
+        store.commit('COMMIT_LOGIN_STATUS', !!data.data)
+      }).catch(() => {
+        store.commit('COMMIT_USER', {})
+        store.commit('COMMIT_LOGIN_STATUS', false)
+      })
+    })
     return {
       authRoute,
       locale: zhCN
