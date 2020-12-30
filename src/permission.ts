b54c2025e1743dad59551839dee92d1bf2dc5b1e
zhangchao <529743595@qq.com>
2020-12-30 16:06:32 +0800

    feat: add router switch scroll top

--- src/permission.ts
+++ src/permission.ts
@@ -1,5 +1,6 @@
 import { router } from '/@/router';
 
 router.afterEach((to, from, next) => {
+  window.scrollTo(0,0);
   document.title = import.meta.env.VITE_APP_TITLE + ' - ' + to.meta.title;
 })
