5eca1068da916bec81d5d52dfba8f9162f94fff9
zhangchao <529743595@qq.com>
2021-01-11 17:36:01 +0800

    feat: split list

---
+++ src/views/Patent/TableList.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="patentList-title">
+    <span>专利名称</span>
+    <span>专利号</span>
+    <span>类型</span>
+    <span>法律状态</span>
+    <span>领域</span>
+    <span>发明日</span>
+    <span>状态</span>
+    <span>零售价(元)</span>
+    <span>VIP加(元)</span>
+  </section>
+  <section class="patentList-content">
+    <div class="patentList-content-item" v-for="patent in patents" :key="patent.number"></div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import UITag from '/@components/UI/UITag.vue';
+import Icon from '/@components/Icon/index.vue';
+import UIButton from '/@components/UI/UIButton.vue';
+import VIPBrand from '/@components/VIPBrand/index.vue';
+import StarIcon from '/@components/StarIcon/index.vue';
+import PreorderButton from '/@components/PreorderButton/index.vue';
+import PatentImage from '/@components/PatentImage/index.vue';
+import PrePatentCountdown from '/@components/PrePatentCountdown/index.vue';
+
+export default defineComponent({
+  name: 'TableList',
+  components: {UITag, Icon, UIButton, VIPBrand, StarIcon, PreorderButton, PatentImage, PrePatentCountdown},
+  props: {
+    patents: {
+      type: Object as PropType<Patent[]>,
+      default: () => ([]),
+    }
+  },
+  setup() {
+    return {
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.patentList {
+  &-title {}
+  &-content {}
+}
+</style>
--- src/views/Patent/index.vue
+++ src/views/Patent/index.vue
@@ -55,45 +55,10 @@
         </div>
       </div>
       <UISpin :spinning="loading">
-        <ul class="patentListBar-list" v-if="patents.length > 0">
-          <li class="patentListBar-list-item" v-for="patent in patents" :key="patent.number">
-            <a target="_blank" :href="`/patent/${patent.number}`">
-              <div class="patentListBar-list-item-image" :class="patent.newest ? 'new' : patent.hot ? 'hot' : ''"><PatentImage :category="patent.category" /></div>
-            </a>
-            <div class="patentListBar-list-item-content">
-              <div class="patentListBar-list-item-content-firstFloor">
-                <a target="_blank" :href="`/patent/${patent.number}`"><b class="patentListBar-list-item-content-firstFloor-title searchKeyword" v-html="patent.nameHighlightKey || patent.name" /></a>
-                <p class="patentListBar-list-item-content-firstFloor-info">
-                  <label v-if="patent.stockStatus === PATENT_STOCK_STATUS.RESERVING"><PrePatentCountdown :patent="patent" /></label>
-                  <StarIcon :patent="patent" />
-                </p>
-              </div>
-              <div class="patentListBar-list-item-content-secondFloor">
-                <p class="patentListBar-list-item-content-secondFloor-des">
-                  <label>专利号：<span class="searchKeyword" v-html="patent.numberHighlightKey || patent.number" /></label>
-                  <label class="patentListBar-list-item-content-secondFloor-des-tags">领域：<span class="searchKeyword" v-for="(tag, index) in (patent.tagsHighlightKey || patent.tags).split(',')" :key="tag"><RouterLink :to="`/patent?word=${encodeURIComponent(patent.tags?.split(',')[index])}`" v-html="tag" />{{index === (patent.tagsHighlightKey || patent.tags).split(',').length - 1 ? '' : '，'}}</span></label>
-                  <label>发明人：{{ patent.inventorExplain }}</label>
-                </p>
-                <p class="patentListBar-list-item-content-secondFloor-des">
-                  <label>专利类型：{{ PATENT_TYPE.label[patent.type] }}</label>
-                  <label>法律状态：{{ patent.legalStatus }}</label>
-                </p>
-              </div>
-              <div class="patentListBar-list-item-content-thirdFloor">
-                <div class="patentListBar-list-item-content-thirdFloor-status">
-                  <span class="patentListBar-list-item-content-thirdFloor-status-tag" :class="patent.certStatus === PATENT_CERT_STATUS.YIZHENG ? 'success' : patent.certStatus === PATENT_CERT_STATUS.WEIZHENG ? 'primary' : 'disabled'">{{ PATENT_CERT_STATUS.label[patent.certStatus] }}</span>
-                  <span class="patentListBar-list-item-content-thirdFloor-status-tag" :class="patent.stockStatus === PATENT_STOCK_STATUS.CAN_SELL ? 'success' : patent.stockStatus === PATENT_STOCK_STATUS.PRE_SELL ? 'primary' : 'disabled'">{{ PATENT_STOCK_STATUS.label[patent.stockStatus] }}</span>
-                </div>
-                <div class="patentListBar-list-item-content-thirdFloor-price">
-                  <label>零售价：<b>￥{{ patent.price }}</b></label>
-                  <VIPBrand class="vipBrand" /><b class="vipPrice">￥<em>{{ patent.vipPrice }}</em></b>
-                  <RouterLink class="buyButton" :to="{path: '/order/confirm', query: {commodityId: patent.id}}"><UIButton :disabled="notActivePatent(patent.stockStatus)" type="primary" customer-class="dangerButton">立即购买</UIButton></RouterLink>
-                  <PreorderButton :disabled="notActivePatent(patent.stockStatus)" :patent="patent" />
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
+        <template v-if="patents.length > 0">
+          <ImageList v-if="listMode === 'imageList'" :patents="patents" />
+          <TableList v-else :patents="patents" />
+        </template>
         <div v-else class="emptyWrapper"><UIEmpty /></div>
       </UISpin>
     </section>
@@ -118,27 +83,24 @@
 import { computed, defineComponent, onMounted, reactive, ref } from 'vue';
 import UITag from '/@components/UI/UITag.vue';
 import Icon from '/@components/Icon/index.vue';
-import VIPBrand from '/@components/VIPBrand/index.vue'
 import UIButton from '/@components/UI/UIButton.vue';
 import UIPagination from '/@components/UI/UIPagination.vue';
 import UISpin from '/@components/UI/UISpin.vue';
 import UIEmpty from '/@components/UI/UIEmpty.vue';
-import StarIcon from '/@components/StarIcon/index.vue'
-import PreorderButton from '/@components/PreorderButton/index.vue'
-import PatentImage from '/@components/PatentImage/index.vue';
 import UITooltip from '/@components/UI/UITooltip.vue';
-import PrePatentCountdown from '/@components/PrePatentCountdown/index.vue';
-import {PATENT_TYPE, PATENT_CERT_STATUS, PATENT_ORIGIN_STATUS, PATENT_STOCK_STATUS} from '/@/utils/dict'
+import { PATENT_TYPE, PATENT_CERT_STATUS, PATENT_ORIGIN_STATUS } from '/@/utils/dict'
 import * as patentApi from '/@api/patent'
 import { useRoute, useRouter, onBeforeRouteUpdate, RouteLocationNormalized } from 'vue-router';
 import { notActivePatent, openNewWidowWithBlob } from '/@/utils';
 import { message } from 'ant-design-vue';
 import { GetPatents } from '/@api/patent';
 import { useStore } from '/@/store';
+import ImageList from '/@/views/Patent/ImageList.vue';
+import TableList from '/@/views/Patent/TableList.vue';
 
 export default defineComponent({
   name: 'Patent',
-  components: {UITag, Icon, VIPBrand, UIButton, UIPagination, UISpin, UIEmpty, StarIcon, PreorderButton, PatentImage, UITooltip, PrePatentCountdown},
+  components: {UITag, Icon, UIButton, UIPagination, UISpin, UIEmpty, UITooltip, ImageList, TableList},
   setup() {
     const store = useStore()
     const route = useRoute()
@@ -221,7 +183,6 @@ export default defineComponent({
       PATENT_TYPE,
       PATENT_CERT_STATUS,
       PATENT_ORIGIN_STATUS,
-      PATENT_STOCK_STATUS,
       filterControl,
       handleFilterControl,
       getPatents,
@@ -308,87 +269,6 @@ export default defineComponent({
         align-items: center;
         > * {margin-left: 10px;font-size: 12px;}
         svg, .fullscreenWrapper {transition: all .3s;cursor: pointer;font-size: 24px;color: #aaa; &:hover {color: #14A8BD;}}
-      }
-    }
-    &-list {
-      &-item {
-        display: flex;
-        padding: 10px 30px;
-        border-bottom: 1px solid #E8E8E8;
-        &-image {
-          width: 118px;
-          overflow: hidden;
-          position: relative;
-          &.new, &.hot {
-            &::before, &::after {
-              font-size: 12px;
-              color: #fff;
-              position: absolute;
-              left: 0; top: 0;
-              width: 0;
-              height: 0;
-            }
-            &::before { content: '';border-width: 19px 10px 6px;border-style: solid;border-color: transparent; }
-            &::after { left: 4px; top: 1px; }
-          }
-          &.new {
-            &::after { content: '新'; }
-            &::before { border-color: #FF5858; border-bottom-color: transparent; }
-          }
-          &.hot {
-            &::before { border-color: #FF9B00; border-bottom-color: transparent; }
-            &::after { content: '热'; }
-          }
-          img {width: 100%;}
-        }
-        &-content {
-          flex: 1;
-          padding-left: 20px;
-          &-firstFloor {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            margin-bottom: 7px;
-            &-title {transition: color .3s;font-size: 16px; &:hover {color: #14A8BD;}}
-            &-info {
-              margin: 0;
-              svg {transition: all .3s;cursor: pointer;font-size: 20px;color: #aaa; &:hover {color: rgb(255,171,31);}}
-              .starIcon {margin-left: .5em}
-              label {margin-left: 2em;}
-            }
-          }
-          &-secondFloor {
-            color: #666666;
-            &-des {
-              margin-bottom: 8px;
-              &:last-child {margin-bottom: 4px;}
-              label {width: 21em; display: inline-block;}
-              &-tags span {transition: color .3s; &:hover {color: #14A8BD;}}
-            }
-          }
-          &-thirdFloor {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            &-status {
-              &-tag {
-                border-radius: 4px;
-                display: inline-block;
-                padding: 0 .6em;
-                font-size: 12px;
-                margin-right: .6em;
-                &.primary {color: #2F86DD;border: 1px solid #2F86DD;}
-                &.success {color: #0DB272;border: 1px solid #0DB272;}
-                &.disabled {color: #999;border: 1px solid #999;}
-              }
-            }
-            &-price {
-              .vipBrand {margin-left: .44em;font-size: 40px;}
-              .vipPrice {color: #FF5858; em {font-size: 24px;font-style: normal;} }
-              .buyButton {vertical-align: top;margin-left: 1.4em;}
-            }
-          }
-        }
       }
     }
   }
