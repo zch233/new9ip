1f7352620d08d6801d3a95f3cb4440d4b90af446
zhangchao <529743595@qq.com>
2021-01-13 16:29:36 +0800

    refactor: clean code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <ConfigProvider :locale="locale">
-    <div class="routerViewWrapper"><RouterView /></div>
-    <SlideArt />
-    <PageFooter :theme="authRoute ? 'light' : 'dark'" />
+    <div class="routerViewWrapper"><RouterView/></div>
+    <SlideArt/>
+    <PageFooter :theme="authRoute ? 'light' : 'dark'"/>
   </ConfigProvider>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, onMounted } from 'vue';
-import {ConfigProvider} from 'ant-design-vue'
+import { ConfigProvider } from 'ant-design-vue';
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
-import PageFooter from '/@components/PageFooter/index.vue'
-import SlideArt from '/@components/SlideArt/index.vue'
+import PageFooter from '/@components/PageFooter/index.vue';
+import SlideArt from '/@components/SlideArt/index.vue';
 import { useRoute } from 'vue-router';
 import * as authApi from '/@api/auth';
 import { useStore } from '/@/store';
@@ -23,26 +23,26 @@ export default defineComponent({
     PageFooter,
     SlideArt,
   },
-  setup() {
-    const route = useRoute()
-    const store = useStore()
-    const authRoute = computed(() => route.path.indexOf('/auth/') >= 0)
+  setup () {
+    const route = useRoute();
+    const store = useStore();
+    const authRoute = computed(() => route.path.indexOf('/auth/') >= 0);
     onMounted(() => {
       authApi.getUserDefault().then(async ({ data }) => {
         if (data.data) {
-          store.commit('COMMIT_LOGIN_STATUS', !!data.data)
-          store.commit('COMMIT_USER', data.data)
-          await store.dispatch('setOneDayConsumePoints')
-          await store.dispatch('setUserPoints')
+          store.commit('COMMIT_LOGIN_STATUS', !!data.data);
+          store.commit('COMMIT_USER', data.data);
+          await store.dispatch('setOneDayConsumePoints');
+          await store.dispatch('setUserPoints');
         }
-      }).catch(() => {})
-    })
+      }).catch(() => {});
+    });
     return {
       authRoute,
       locale: zhCN,
-    }
-  }
-})
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
