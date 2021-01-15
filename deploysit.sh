da041b3d41132494f11b52dcedc779b044d9d48a
zhangchao <529743595@qq.com>
2021-01-15 09:17:20 +0800

    feat: update deploy sh

--- deploysit.sh
+++ deploysit.sh
@@ -4,5 +4,5 @@ scp -P 20430 -r ./dist/. root@49.235.115.239:/www/9ip
 time=$(date "+%Y.%m.%d-%H.%M.%S") &&
 echo "发布成功：${time}" &&
 rm -rf dist &&
-git add . &&
+git rm -rf dist &&
 git commit -m "deploy:sit"
