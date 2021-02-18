d3dd880817ffa43a7298476ce6e13eef24cfa385
zhangchao <529743595@qq.com>
2021-02-18 14:20:22 +0800

    styles: add antd theme

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,47 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import QRCode from 'qrcode';
 import path from 'path';
+import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: '/',
-  // @ts-ignore
-  plugins: [vue(), QRCode],
+  plugins: [
+    vue(),
+    // @ts-ignore
+    QRCode,
+    styleImport({
+      libs: [{
+        libraryName: 'ant-design-vue',
+        esModule: true,
+        resolveStyle: (name) => {
+          return `ant-design-vue/es/${name}/style/index.less`;
+        },
+      }]
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          'primary-color': '#14A8BD', // 全局主色
+          'link-color': '#14A8BD', // 链接色
+          'success-color': '#52c41a', // 成功色
+          'warning-color': '#FFBB12', // 警告色
+          'error-color': '#FF5858', // 错误色
+          'font-size-base': '14px', // 主字号
+          'heading-color': 'rgba(0, 0, 0, 0.85)', // 标题色
+          'text-color': 'rgba(0, 0, 0, 0.65)', // 主文本色
+          'text-color-secondary': 'rgba(0, 0, 0, 0.45)', // 次文本色
+          'disabled-color': 'rgba(0, 0, 0, 0.25)', // 失效色
+          'border-radius-base': '2px', // 组件/浮层圆角
+          'border-color-base': '#DEDEDE', // 边框色
+          'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)', // 浮层阴影
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '/@': path.resolve(__dirname, './src'),
