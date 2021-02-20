61b3176deeff3ec1e9fa87abbc3ebf3d1d13d782
zhangchao <529743595@qq.com>
2021-02-20 17:27:23 +0800

    fix: fix build:sit sh

--- deploysit.sh
+++ deploysit.sh
@@ -1,6 +1,6 @@
 yarn build:sit &&
 echo "打包成功，正在发布服务器49.235.115.239..." &&
-scp -P 20430 -r ./dist/. root@49.235.115.239:/www/9ip
+scp -P 20430 -r ./dist/. root@49.235.115.239:/www/9ip &&
 time=$(date "+%Y.%m.%d-%H.%M.%S") &&
 echo "发布成功：${time}" &&
 git add dist &&
