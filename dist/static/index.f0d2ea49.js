f62ffd8c65d439b5ae2cdd8e8225c9c037a953c5
zhangchao <529743595@qq.com>
2021-03-11 11:03:59 +0800

    deploy:sit

---
+++ dist/static/index.f0d2ea49.js
@@ -0,0 +1,1 @@
+import{d as a,i as r,j as s,q as e}from"./index.29c376da.js";var d=a({name:"PatentImage",props:{cover:String}});const t=e("data-v-05bd0196")(((a,e,d,t,o,c)=>(r(),s("img",{src:a.cover,alt:""},null,8,["src"]))));d.render=t,d.__scopeId="data-v-05bd0196";export{d as _};
